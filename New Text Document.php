ec020e2d85735801b8861b4c64aa894465b257cd
Ahmede Sidi Abderrahmane <ahmedesidiabderrahmane@gmail.com>
2024-10-19 15:15:35 +0200

    Add files via upload

---
+++ New Text Document.php
@@ -0,0 +1,23 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['fullname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // The email where you'll receive the contact form submissions
+    $to = "ahmedesidiabderrahmane@gmail.com";
+    $subject = "Contact Form Submission from $name";
+    $body = "You have received a new message from your website contact form.\n\n".
+            "Name: $name\n".
+            "Email: $email\n".
+            "Message:\n$message";
+    $headers = "From: $email";
+
+    // Send the email
+    if(mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send the message.";
+    }
+}
+?>
